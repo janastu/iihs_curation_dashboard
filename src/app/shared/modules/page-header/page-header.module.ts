6b1a05d2d8b5a6c3b632f3ca5c077eeb7640028b
vaishnavi <vaishnavisv001@gmail.com>
2017-11-01 11:58:37 +0530

    nav bar layout

--- src/app/shared/modules/page-header/page-header.module.ts
+++ src/app/shared/modules/page-header/page-header.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PageHeaderComponent } from './page-header.component';
 //import { DropdownComponent } from '../../../layout/bs-component/components';
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        NgbModule.forRoot()
     ],
     declarations: [PageHeaderComponent],
     exports: [PageHeaderComponent]
