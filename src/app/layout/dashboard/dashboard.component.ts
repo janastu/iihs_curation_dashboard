ee40b05744f418577d88f2be2f21d0ca6fbfa0c1
Madhuvanthi-A <amadhuvanthi@gmail.com>
2017-11-22 16:13:30 +0530

    #40 user management info is displayed

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+    import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validators, FormGroup} from '@angular/forms';
 import { routerTransition } from '../../router.animations';
 import { Service } from '../../services/services';
 @Component({
@@ -9,7 +10,13 @@ import { Service } from '../../services/services';
 })
 export class DashboardComponent implements OnInit {
 	feeds:any=[];
-    constructor(public service:Service) {
+    contacts:any=[];
+    usertypes:any=[{name:'admin'},{name:'student'}];
+    //loginForm:FormGroup;
+    //name = this.formBuilder.control('', [Validators.required]);
+    //mail = this.formBuilder.control('', [Validators.required]);
+   usertype = this.formBuilder.control('', [Validators.required]);
+    constructor(public service:Service, public formBuilder:FormBuilder) {
     }
 
     ngOnInit() {
@@ -17,7 +24,29 @@ export class DashboardComponent implements OnInit {
 
 this.feeds= result["_nr_stories"];
 console.log("feeds",this.feeds);
-});
+})
     }
-   
+    
+
+     addContact(name,mail,user,phone){
+        let contact = new Contact(name,mail,user);
+        this.contacts.push(contact);
+    }
+     removeContact(contact){
+        let index = this.contacts.indexOf(contact);
+        this.contacts.splice(index,1);
+    }
+}
+export class Contact{
+    name:string;
+    mail:string;
+    user:string;
+    descr:string;
+
+    constructor(name,mail,user){
+        this.name=name;
+        this.mail=mail;
+        this.user=user;
+        this.descr=this.name+'   '+this.mail+'   '+this.user;
+    }
 }
