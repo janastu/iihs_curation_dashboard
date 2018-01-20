be72eecf2c757aeb8237fc995c1cd36f2c5fa77d
Jabiulla Mulla <jabiulla24@gmail.com>
2018-01-20 19:01:05 +0530

    route hashpath

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { CategoryService } from './services/category-service';
 import { DataService } from './services/data-service';
 import { ComponentsService } from './services/components-service';
 import { Service } from './services/services';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { FeedService } from './services/feed-service';
 
@@ -55,7 +56,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         
     ],
 
-    providers: [AuthGuard,Global,HttpClient,JsonConvert,BoardService,CategoryService,DataService,ComponentsService,Service,Settings,Userservice,FeedService],
+    providers: [{provide: LocationStrategy, useClass: HashLocationStrategy},AuthGuard,Global,HttpClient,JsonConvert,BoardService,CategoryService,DataService,ComponentsService,Service,Settings,Userservice,FeedService],
 
     bootstrap: [AppComponent]
 })
