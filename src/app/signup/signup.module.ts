366e6b91bf5c113b488e6d8d1ab66f9f53c79fae
vaishnavi <vaishnavisv001@gmail.com>
2018-02-22 21:25:09 +0530

    #179 resolved

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
-
+//import { AppFieldErrorDisplayModule } from '../sharedfeeds/components';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   imports: [
@@ -11,6 +11,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     SignupRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+   // AppFieldErrorDisplayModule,
     NgbModule.forRoot()
   ],
   declarations: [SignupComponent]
