34317771d3ee38f60133ca130545d51d9ac8f693
Jabiulla Mulla <jabiulla24@gmail.com>
2018-02-23 17:07:12 +0530

    issue #184 backend

---
+++ src/app/resetpassword/resetpassword-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ResetpasswordComponent } from './resetpassword.component';
+
+const routes: Routes = [
+    { path: '', component: ResetpasswordComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ResetpasswordRoutingModule { }
